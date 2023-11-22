03f42c0841b159bbd72743c19ff69841af2c63ca
Leonard Schwennesen <leonard.schwennesen@eu.rhenus.com>
2023-11-22 13:32:56 +0100

    add minimal page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,31 @@
-/** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+"use client";
+
+import { useResizeDetector } from "react-resize-detector";
+
+export default function HomePage() {
+  const { width, height, ref } = useResizeDetector();
+
+  return (
+    <main
+      style={{
+        display: "flex",
+      }}
+    >
+      <div
+        style={{
+          width: `${width}px`,
+          height: `${height}px`,
+          background: "green",
+        }}
+      ></div>
+      <div
+        style={{
+          width: "256px",
+          height: "256px",
+          background: "red",
+        }}
+        ref={ref}
+      ></div>
+    </main>
+  );
 }
